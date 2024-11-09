c4af2f815ea45566f1ddee0458d4c4dac54e6122
Amaan-Samar <80666102+Amaan-Samar@users.noreply.github.com>
2024-11-09 07:09:52 +0000

    Deploying to gh-pages from @ Amaan-Samar/chinese-writing-assistant@cd061675588a9ac67ff739d7d696f5046c05098a 🚀

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/chinese-writing-assistant/sw.js', { scope: '/chinese-writing-assistant/' })})}
